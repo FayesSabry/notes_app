7cbecd7041c4bca6cb188beff47fe72a6f5c28cd
FayezSabry <georgesabry35@gmail.com>
2025-04-12 13:54:31 +0200

    finish notes bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,13 +10,44 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextField(hintText: 'Title'),
-          SizedBox(height: 16),
-          CustomTextField(hintText: 'Content', maxLines: 4),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextField(hintText: 'Title'),
+            SizedBox(height: 16),
+            CustomTextField(hintText: 'Content', maxLines: 4),
+            SizedBox(height: 24),
+            CustomBottn(text: 'Add'),
+            SizedBox(height: 15),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomBottn extends StatelessWidget {
+  const CustomBottn({super.key, required this.text,this.onTap});
+  final String text;
+  final void Function()? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: 1.sw,
+        height: 55.h,
+        decoration: BoxDecoration(
+          color: kPriamaryColor,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 20.sp, color: Colors.black),
+          ),
+        ),
       ),
     );
   }
