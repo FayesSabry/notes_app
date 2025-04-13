92c1414f74e8a4e00770239c798a4bc3c8213e7d
FayezSabry <georgesabry35@gmail.com>
2025-04-13 13:08:02 +0200

    create note cubit

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -10,25 +10,60 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.0),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 32),
-            CustomTextField(hintText: 'Title'),
-            SizedBox(height: 16),
-            CustomTextField(hintText: 'Content', maxLines: 4),
-            SizedBox(height: 24),
-            CustomBottn(text: 'Add'),
-            SizedBox(height: 15),
-          ],
-        ),
+      child: SingleChildScrollView(child: AddNoteForm()),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({super.key});
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title, content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          const SizedBox(height: 32),
+          CustomTextField(hintText: 'Title', onSaved: (value) => title = value),
+          const SizedBox(height: 16),
+          CustomTextField(
+            hintText: 'Content',
+            maxLines: 4,
+            onSaved: (value) => content = value,
+          ),
+          const SizedBox(height: 24),
+          CustomBottn(
+            text: 'Add',
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(height: 200),
+        ],
       ),
     );
   }
 }
 
 class CustomBottn extends StatelessWidget {
-  const CustomBottn({super.key, required this.text,this.onTap});
+  const CustomBottn({super.key, required this.text, this.onTap});
   final String text;
   final void Function()? onTap;
   @override
