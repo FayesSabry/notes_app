528df6db39c1a6a6c492c66c35bd225352810456
FayezSabry <georgesabry35@gmail.com>
2025-04-13 13:33:42 +0200

    Provide addNote cubit

--- lib/routing/router_generation.dart
+++ lib/routing/router_generation.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/routing/app_routes.dart';
 import 'package:notes_app/views/edit_view.dart';
 import 'package:notes_app/views/notes_view.dart';
@@ -10,7 +12,11 @@ class RouterGeneration {
       GoRoute(
         path: AppRoutes.notesView,
         name: AppRoutes.notesView,
-        builder: (context, state) => const NotesView(),
+        builder:
+            (context, state) => BlocProvider(
+              create: (context) => AddNoteCubit(),
+              child: const NotesView(),
+            ),
       ),
       GoRoute(
         path: AppRoutes.editView,
